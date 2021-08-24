36e58565a3c10937653e31ae76122cc290423b6e
Edwin Brandon Lopez Martinez <k9999-f@hotmail.com>
2021-08-24 00:08:24 -0500

    Subiendo archivos iniciales

---
+++ js/change-img.js
@@ -0,0 +1,123 @@
+$(document).ready(function() {
+    
+    /******** Lancer Evolution ***********/
+    $( "img#lancerEvo" ).mouseenter(function() {
+        $(this).attr('src', 'img/Lancer2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Lancer1.jpg');
+    });
+    
+    /*************** L200 *************/
+    $( "img#L200" ).mouseenter(function() {
+        $(this).attr('src', 'img/L2002.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/L2001.jpg');
+    });
+    
+    /*************** Eclipce *************/
+    $( "img#Eclipse" ).mouseenter(function() {
+        $(this).attr('src', 'img/Eclipce2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Eclipce1.jpg');
+    });
+    
+    /*************** Navara *************/
+    $( "img#Navara" ).mouseenter(function() {
+        $(this).attr('src', 'img/Navara2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Navara1.jpg');
+    });
+    
+    /*************** Frontier *************/
+    $( "img#Frontier" ).mouseenter(function() {
+        $(this).attr('src', 'img/Frontier2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Frontier1.jpg');
+    });
+    
+    /*************** Hilux *************/
+    $( "img#Hilux" ).mouseenter(function() {
+        $(this).attr('src', 'img/Hilux2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Hilux1.jpg');
+    });
+    
+    /*************** Celica *************/
+    $( "img#Celica" ).mouseenter(function() {
+        $(this).attr('src', 'img/Celica2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Celica1.jpg');
+    });
+    
+    /*************** Supra *************/
+    $( "img#Supra" ).mouseenter(function() {
+        $(this).attr('src', 'img/Supra2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Supra1.jpg');
+    });
+    
+    /*************** Civic *************/
+    $( "img#Civic" ).mouseenter(function() {
+        $(this).attr('src', 'img/Civic2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Civic1.jpg');
+    });
+    
+    /*************** CR-V *************/
+    $( "img#Crv" ).mouseenter(function() {
+        $(this).attr('src', 'img/Cr-v2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Cr-v1.jpg');
+    });
+    
+    /*************** Mazda 3 *************/
+    $( "img#Mazda3" ).mouseenter(function() {
+        $(this).attr('src', 'img/Mazda32.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Mazda31.jpg');
+    });
+    
+    /*************** Mazda RX-7 *************/
+    $( "img#Rx-7" ).mouseenter(function() {
+        $(this).attr('src', 'img/Rx-72.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Rx-71.jpg');
+    });
+    
+    /*************** Mazda RX-8 *************/
+    $( "img#Rx-8" ).mouseenter(function() {
+        $(this).attr('src', 'img/Rx-82.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Rx-81.jpg');
+    });
+    
+    /*************** Impreza *************/
+    $( "img#Impreza" ).mouseenter(function() {
+        $(this).attr('src', 'img/Impreza2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Impreza1.jpg');
+    });
+    
+    /*************** XV *************/
+    $( "img#Xv" ).mouseenter(function() {
+        $(this).attr('src', 'img/Xv2.jpg');
+    })
+    .mouseleave(function() {
+        $(this).attr('src', 'img/Xv1.jpg');
+    });
+    
+});
---
+++ js/theme.js
@@ -0,0 +1,273 @@
+$(document).ready(function() {
+    /************* Dark-theme*************/
+    $( "#dark-theme" ).click(function() {
+        $('nav').css({
+            'background-color':'rgba(18, 28, 37,1)',
+            'color':'#fff'
+        });
+        $('p.icon').css({
+            'color':'#fff'
+        });
+        $('footer').css({
+            'background-color':'rgba(18, 28, 37,1)',
+            'color':'#fff'
+        });
+        $('#container-themes').css({
+            'background-color':'rgba(18, 28, 37,1)'
+        });
+        $('#icon-palette').css({
+            'background-color':'rgba(18, 28, 37,1)'
+        });
+        $('#tittle-palette').css({
+            'background-color':'rgba(18, 28, 37,1)',
+            'color':'#fff'
+        });
+        $('.description').css({
+            'background-color':'rgba(0, 0, 0,.97)',
+            'color':'#fff'
+        });
+        $('.textTittles').css({
+            'font-family':'Dark'
+        });
+        $('div#container-menu-mobile').css({
+            'background-color':'rgba(18, 28, 37,1)',
+            'color':'#fff',
+            'border-top': 'solid 1px rgba(255,255,255,.2)'
+        });
+        $('.body-content').css({
+            'background-color':'rgba(236, 240, 241,.9)',
+            'color':'#000'
+        });
+        $('#button-menu-mobil').css({
+            'color':'#fff'
+        });
+        $('a.link-mobile-menu').css({
+            'color':'#fff',
+            'border-bottom':'1px solid rgba(255,255,255,.2)'
+        });
+        $('button#button-close-menu-mobile').css({
+            'color':'#fff'
+        });
+        $('#container-themes').css({
+            'border-top':'1px solid rgba(255,255,255,.1)'
+        });
+    });
+    
+    /************* Ocean-theme*************/
+    $( "#ocean-theme" ).click(function() {
+        $('nav').css({
+            'background-color':'rgb(0, 187, 241)',
+            'color':'#fff'
+        });
+        $('p.icon').css({
+            'color':'#fff'
+        });
+        $('footer').css({
+            'background-color':'rgb(0, 187, 241)',
+            'color':'#fff'
+        });
+        $('#container-themes').css({
+            'background-color':'rgb(0, 187, 241)'
+        });
+        $('#icon-palette').css({
+            'background-color':'rgb(0, 187, 241)'
+        });
+        $('#tittle-palette').css({
+            'background-color':'rgb(0, 187, 241)',
+            'color':'#fff'
+        });
+        $('.description').css({
+            'background-color':'rgba(0, 187, 241,.8)',
+            'color':'#000'
+        });
+        $('.textTittles').css({
+            'font-family':'Ocean'
+        });
+        $('div#container-menu-mobile').css({
+            'background-color':'rgb(0, 187, 241)',
+            'color':'#fff',
+            'border-top': 'solid 1px rgba(255,255,255,.2)'
+        });
+        $('.body-content').css({
+            'background-color':'rgba(236, 240, 241,.9)',
+            'color':'#000'
+        });
+        $('#button-menu-mobil').css({
+            'color':'#fff'
+        });
+        $('a.link-mobile-menu').css({
+            'color':'#fff',
+            'border-bottom':'1px solid rgba(255,255,255,.2)'
+        });
+        $('button#button-close-menu-mobile').css({
+            'color':'#fff'
+        });
+        $('#container-themes').css({
+            'border-top':'1px solid rgba(255,255,255,.1)'
+        });
+    });
+    
+    /************* Fire-theme*************/
+    $( "#fire-theme" ).click(function() {
+        $('nav').css({
+            'background-color':'rgb(178, 14, 33)',
+            'color':'#fff'
+        });
+        $('p.icon').css({
+            'color':'#fff'
+        });
+        $('footer').css({
+            'background-color':'rgb(178, 14, 33)',
+            'color':'#fff'
+        });
+        $('#container-themes').css({
+            'background-color':'rgb(178, 14, 33)'
+        });
+        $('#icon-palette').css({
+            'background-color':'rgb(178, 14, 33)'
+        });
+        $('#tittle-palette').css({
+            'background-color':'rgb(178, 14, 33)',
+            'color':'#fff'
+        });
+        $('.description').css({
+            'background-color':'rgba(178, 14, 33,.7)',
+            'color':'#000'
+        });
+        $('.textTittles').css({
+            'font-family':'Fire'
+        });
+        $('div#container-menu-mobile').css({
+            'background-color':'rgb(178, 14, 33)',
+            'color':'#fff',
+            'border-top': 'solid 1px rgba(255,255,255,.2)'
+        });
+        $('.body-content').css({
+            'background-color':'rgba(236, 240, 241,.9)',
+            'color':'#000'
+        });
+        $('#button-menu-mobil').css({
+            'color':'#fff'
+        });
+        $('a.link-mobile-menu').css({
+            'color':'#fff',
+            'border-bottom':'1px solid rgba(255,255,255,.2)'
+        });
+        $('button#button-close-menu-mobile').css({
+            'color':'#fff'
+        });
+        $('#container-themes').css({
+            'border-top':'1px solid rgba(255,255,255,.1)'
+        });
+    });
+    
+    /************* Nature-theme*************/
+    $( "#nature-theme" ).click(function() {
+        $('nav').css({
+            'background-color':'rgb(20, 102, 61)',
+            'color':'#fff'
+        });
+        $('p.icon').css({
+            'color':'#fff'
+        });
+        $('footer').css({
+            'background-color':'rgb(20, 102, 61)',
+            'color':'#fff'
+        });
+        $('#container-themes').css({
+            'background-color':'rgb(20, 102, 61)'
+        });
+        $('#icon-palette').css({
+            'background-color':'rgb(20, 102, 61)'
+        });
+        $('#tittle-palette').css({
+            'background-color':'rgb(20, 102, 61)',
+            'color':'#fff'
+        });
+        $('.description').css({
+            'background-color':'rgba(20, 102, 61,.8)',
+            'color':'#000'
+        });
+        $('.textTittles').css({
+            'font-family':'Nature'
+        });
+        $('div#container-menu-mobile').css({
+            'background-color':'rgb(20, 102, 61)',
+            'color':'#fff',
+            'border-top': 'solid 1px rgba(255,255,255,.2)'
+        });
+        $('.body-content').css({
+            'background-color':'rgba(236, 240, 241,.9)',
+            'color':'#000'
+        });
+        $('#button-menu-mobil').css({
+            'color':'#fff'
+        });
+        $('a.link-mobile-menu').css({
+            'color':'#fff',
+            'border-bottom':'1px solid rgba(255,255,255,.2)'
+        });
+        $('button#button-close-menu-mobile').css({
+            'color':'#fff'
+        });
+        $('#container-themes').css({
+            'border-top':'1px solid rgba(255,255,255,.1)'
+        });
+    });
+    
+    /************* Sky-theme*************/
+    $( "#sky-theme" ).click(function() {
+        $('nav').css({
+            'background-color':'rgb(255, 255, 255)',
+            'color':'#000'
+        });
+        $('p.icon').css({
+            'color':'#000'
+        });
+        $('footer').css({
+            'background-color':'rgb(255, 255, 255)',
+            'color':'#000'
+        });
+        $('.body-content').css({
+            'background-color':'rgba(165, 229, 247,.97)',
+            'color':'#000'
+        });
+        $('#container-themes').css({
+            'background-color':'rgb(255, 255, 255)'
+        });
+        $('#icon-palette').css({
+            'background-color':'rgb(255, 255, 255)'
+        });
+        $('#tittle-palette').css({
+            'background-color':'rgb(255, 255, 255)',
+            'color':'#000'
+        });
+        $('.description').css({
+            'background-color':'rgba(255, 255, 255,.8)',
+            'color':'#000'
+        });
+        $('.textTittles').css({
+            'font-family':'Sky'
+        });
+        $('div#container-menu-mobile').css({
+            'background-color':'rgb(255, 255, 255)',
+            'color':'#000',
+            'border-top': 'solid 1px rgba(0,0,0,.1)'
+        });
+        $('#button-menu-mobil').css({
+            'color':'#000'
+        });
+        $('a.link-mobile-menu').css({
+            'color':'#000',
+            'border-bottom':'1px solid rgba(0,0,0,.2)'
+        });
+        $('button#button-close-menu-mobile').css({
+            'color':'#000'
+        });
+        $('#container-themes').css({
+            'border-top':'1px solid rgba(0,0,0,.1)'
+        });
+    });
+    
+    
+});
